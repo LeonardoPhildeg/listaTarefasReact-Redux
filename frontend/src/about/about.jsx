8c2829c8b24bc15cfdd219c7c8b8c6e1c885d344
leonardophildeg <leonardo.phildeg@bridge.ufsc.br>
2019-04-29 19:25:11 -0300

    terminando

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PageReader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <h1>
+        <PageReader name='Sobre' small='Nós'/>
+        </h1>
+    </div>
+)
